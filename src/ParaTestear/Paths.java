c9390ca3cab4e0d19dc6f54f2c779531b382739c
Nicolas Guasch <nic.guasch@gmail.com>
2018-09-08 00:58:15 -0300

    Relative paths

--- src/ParaTestear/Paths.java
+++ src/ParaTestear/Paths.java
@@ -1,10 +1,10 @@
 package ParaTestear;
 
 public class Paths
-{
-    public static final String NaveTester = "C:\\Users\\alvmi\\Documents\\GitHub\\ProyectoTDP\\src\\ParaTestear\\bv.png";
-    public static final String Alcon = "C:\\Users\\alvmi\\Documents\\GitHub\\ProyectoTDP\\src\\ParaTestear\\alcon.png";
-    public static final String Bala = "C:\\Users\\alvmi\\Documents\\GitHub\\ProyectoTDP\\src\\ParaTestear\\bala.png";
-    public static final String Background = "C:\\Users\\alvmi\\Documents\\GitHub\\ProyectoTDP\\src\\ParaTestear\\fondo.png";
-    public static final String DeathStar = "C:\\Users\\alvmi\\Documents\\GitHub\\ProyectoTDP\\src\\ParaTestear\\deathstar.png";
+{//TODO: Usar URL y getClass().getResource()
+    public static final String NaveTester = "src/ParaTestear/bv.png";
+    public static final String Alcon = "src/ParaTestear/alcon.png";
+    public static final String Bala = "src/ParaTestear/bala.png";
+    public static final String Background = "src/ParaTestear/fondo.png";
+    public static final String DeathStar = "src/ParaTestear/deathstar.png";
 }
