81d7fa1c40f7f7f96b0c0c0f4ff63c1277a3e0c2
Nicolas Guasch <nic.guasch@gmail.com>
2018-09-27 10:16:24 -0300

    Replaced Stuff.path with Assets.AssetStore

---
+++ ProyectoTDP/src/Assets/AssetsManager.java
@@ -0,0 +1,32 @@
+package Assets;
+import java.net.URL;
+
+public class AssetsManager
+{
+
+    // ---------- SPRITES ---------------
+    public static final URL NaveTester = AssetsManager.class.getResource("bv.png");
+    public static final URL Alcon = AssetsManager.class.getResource("alcon.png");
+    public static final URL Bala = AssetsManager.class.getResource("bala.png");
+    public static final URL Background = AssetsManager.class.getResource("fondo.png");
+    public static final URL DeathStar = AssetsManager.class.getResource("deathstar.png");
+    public static final URL Vader = AssetsManager.class.getResource("vader_normal.png");
+    public static final URL TIE = AssetsManager.class.getResource("tie_normal.png");
+    public static final URL TIE2 = AssetsManager.class.getResource("tie2.png");
+    public static final URL TIE3 = AssetsManager.class.getResource("tie3.png");
+    public static final URL TIELIL = AssetsManager.class.getResource("liltie.png");
+    public static final URL TIESUPER = AssetsManager.class.getResource("supertie.png");
+    public static final URL WHITETIE = AssetsManager.class.getResource("whitetie.png");
+    public static final URL Laser = AssetsManager.class.getResource("laser.png");
+    public static final URL LaserBlue = AssetsManager.class.getResource("laserblue.png");
+    public static final URL LaserRed = AssetsManager.class.getResource("laserrojo.png");
+    public static final URL Pause = AssetsManager.class.getResource("wooky.png");
+    public static final URL Level1 = AssetsManager.class.getResource("levintro1.png");
+    public static final URL Level2 = AssetsManager.class.getResource("levintro2.png");
+    public static final URL GameOver = AssetsManager.class.getResource("gameover.png");
+    public static final URL LIFE = AssetsManager.class.getResource("life.png");
+    public static final URL MonedaArma = AssetsManager.class.getResource("monedaarma.gif");
+
+
+
+}
---
+++ ProyectoTDP/src/Audio/AudioManager.java
@@ -0,0 +1,12 @@
+package Audio;
+import java.net.URL;
+
+public class AudioManager{
+
+    public static final URL Pew = AudioManager.class.getResource("pew.wav");
+    public static final URL Pew2 = AudioManager.class.getResource("pew2.wav");
+    public static final URL ImperialMarch = AudioManager.class.getResource("musicimper.wav");
+    public static final URL Menu = AudioManager.class.getResource("menu.wav");
+    public static final URL TieAttack = AudioManager.class.getResource("tieroar.wav");
+    public static final URL VaderBreath = AudioManager.class.getResource("vaderbreath.wav");
+}
