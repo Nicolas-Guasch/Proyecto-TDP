d75f23c81683dce0b6546fa438f6afc66eb2d363
alvmivan@gmail.com <wallywest>
2018-10-30 00:33:45 -0300

    working on filesys

--- ProyectoTDP/src/FileSys/MonoThreadFileManager.java
+++ ProyectoTDP/src/FileSys/MonoThreadFileManager.java
@@ -1,17 +1,45 @@
 package FileSys;
 
+import java.io.File;
+import java.util.*;
+
 class MonoThreadFileManager implements IFileManager {
 
+    // name -> content
+    private Map<String,String> lightweightLoads = new TreeMap<>();
+
+    public static String[] commonExtensions = {".txt",".meta",".settings",".json"};
 
 
-    @Override
-    public synchronized void save(String path, String text, boolean append) {
+    MonoThreadFileManager(){
 
     }
 
+
+
+
+    private static String[] usualDirs = {
+                "Assets\\Animators\\",
+                "Assets\\Bullets\\",
+                "Assets\\Ships\\",
+                "Assets\\Stuff\\",
+                "Assets\\UI\\",
+                "Assets\\",
+                };
+    private Collection<String> currentsWaysToFindAFile(){
+
+    }
+
+
     @Override
     public synchronized String load(String path) {
-        return null;
+        if(lightweightLoads.containsKey(path.toLowerCase())){
+            return lightweightLoads.get(path.toLowerCase());
+        }
+        StringBuffer current = new StringBuffer();
+
+
+
     }
 
     @Override
@@ -22,5 +50,11 @@ class MonoThreadFileManager implements IFileManager {
     @Override
     public synchronized Iterable<String> loadSplitedByRegex(String path, String regex) {
         return null;
+    }
+
+
+    @Override
+    public synchronized void save(String path, String text, boolean append) {
+
     }
 }
