3ff49c72a1532f1b77d59c9c996a4063af79e8d9
Nicolas Guasch <nic.guasch@gmail.com>
2018-11-07 23:53:04 -0300

    GameData UML

--- ProyectoTDP/src/GameData/FileSettingsParser.java
+++ ProyectoTDP/src/GameData/FileSettingsParser.java
@@ -1,7 +1,6 @@
 package GameData;
 
 
-import Entities.Entity;
 import Entities.EntityData;
 
 import javax.swing.*;
@@ -14,8 +13,8 @@ import java.util.regex.*;
 public class FileSettingsParser implements ISettingsParser{
     private int FPS;
     private Dimension sizeWindow;
-    Map<FloatEnum,Float>floats;
-    Map<EntityEnum,EntityData>entities;
+    Map<FloatEnum,Float> floatInput;
+    Map<EntityEnum,EntityData> entitiyDataInput;
 
     public FileSettingsParser(){
         Pattern pFloats = Pattern.compile("(\\w+): (\\d+\\.?\\d*f?)");
@@ -40,21 +39,21 @@ public class FileSettingsParser implements ISettingsParser{
             if(!window.matches())failure();
             sizeWindow = new Dimension(Integer.parseInt(window.group(1)), Integer.parseInt(window.group(2)));
 
-            floats = new TreeMap<>();
-            entities = new TreeMap<>();
+            floatInput = new TreeMap<>();
+            entitiyDataInput = new TreeMap<>();
             while(inputBuffer.ready()){
                 line = inputBuffer.readLine();
                 Matcher mFloat = pFloats.matcher(line);
                 Matcher mEntity = pEntity.matcher(line);
                 if(mFloat.matches()&& floatVal.containsKey(mFloat.group(1))){
-                    floats.putIfAbsent(floatVal.get(mFloat.group(1)),Float.parseFloat(mFloat.group(2)));
+                    floatInput.putIfAbsent(floatVal.get(mFloat.group(1)),Float.parseFloat(mFloat.group(2)));
                 }
                 else if(mEntity.matches()&& entityVal.containsKey(mEntity.group(1))){
                     float h,d,s;
                     h = Float.parseFloat(mEntity.group(2));
                     d = Float.parseFloat(mEntity.group(3));
                     s = Float.parseFloat(mEntity.group(4));
-                    entities.putIfAbsent(entityVal.get(mEntity.group(1)),new EntityData(h,d,s));
+                    entitiyDataInput.putIfAbsent(entityVal.get(mEntity.group(1)),new EntityData(h,d,s));
                 }
             }
         }catch (IOException | IndexOutOfBoundsException e){
@@ -66,8 +65,8 @@ public class FileSettingsParser implements ISettingsParser{
             }catch(IOException e){
                 failure();
             }
-            for(Map.Entry<FloatEnum,Float> f:floats.entrySet())System.out.println(f);
-            for(Map.Entry<EntityEnum,EntityData> e:entities.entrySet())System.out.println(e);
+            for(Map.Entry<FloatEnum,Float> f: floatInput.entrySet())System.out.println(f);
+            for(Map.Entry<EntityEnum,EntityData> e: entitiyDataInput.entrySet())System.out.println(e);
         }
 
 
@@ -79,12 +78,12 @@ public class FileSettingsParser implements ISettingsParser{
     }
 
     public EntityData getEntityData(EntityEnum ref){
-        if(!entities.containsKey(ref))System.out.println(ref);
-       return entities.getOrDefault(ref,new EntityData(0,0,0));
+        if(!entitiyDataInput.containsKey(ref))System.out.println(ref);
+       return entitiyDataInput.getOrDefault(ref,new EntityData(0,0,0));
     }
     public float getFloat(FloatEnum ref){
-        if(!floats  .containsKey(ref))System.out.println(ref);
-        return floats.getOrDefault(ref,0f);
+        if(!floatInput.containsKey(ref))System.out.println(ref);
+        return floatInput.getOrDefault(ref,0f);
     }
     public int FPS(){
         return FPS;
