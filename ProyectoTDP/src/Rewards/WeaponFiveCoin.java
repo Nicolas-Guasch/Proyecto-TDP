69a7145a8e725604c161a577d2dfb0fcfc3d90f8
alvmivan@gmail.com <wallywest>
2018-11-29 12:24:29 -0300

    nanananananananananananananana BATAMAAAAANNNN! -a

--- ProyectoTDP/src/Rewards/WeaponFiveCoin.java
+++ ProyectoTDP/src/Rewards/WeaponFiveCoin.java
@@ -18,16 +18,16 @@ public class WeaponFiveCoin implements java.util.function.Consumer<ITransform> {
     @Override
     public void accept(ITransform ITransform) {
         PlayerShip player = PlayerShip.getInstance();
-        IGameObject IGameObject = GameObject.getRoot().addChild();
+        IGameObject referenced = GameObject.getRoot().addChild();
         SpriteData spriteData = new SpriteData("5reward");
-        WeaponReward reward = new WeaponReward(IGameObject,spriteData);
+        WeaponReward reward = new WeaponReward(referenced,spriteData);
         BulletDirector<PlayerBullet, PlayerBulletBuilder> director = new BulletDirector<PlayerBullet, PlayerBulletBuilder>();
         director.setBuilder(new BulletPlayerBuilder(player.referenced().transform()));
         Weapon weapon = new AngularWeapon<BulletDirector<PlayerBullet, PlayerBulletBuilder>>(player.referenced().transform(),director, 5);
         weapon.setName("penta");
         reward.setWeapon(weapon);
-        IGameObject.transform().setPosition(ITransform.position3());
-        IGameObject.addComponent(new RewardMove());
+        referenced.transform().setPosition(ITransform.position3());
+        referenced.addComponent(new RewardMove());
         reward.setData(EntityData.WithEqualsValues(100));
         EveryOne.getInstance().add(reward);
     }
